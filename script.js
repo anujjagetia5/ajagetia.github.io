62492bf8e7532d2bb0758d645e7cbd3ec01e7be5
anujjagetia5 <ajagetia@wpi.edu>
2025-04-24 15:51:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -83,3 +83,13 @@ window.addEventListener('scroll', () => {
         navContainer.style.boxShadow = 'none';
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menuToggle');
+    const navLinks = document.getElementById('navLinks');
+
+    menuToggle.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+});
